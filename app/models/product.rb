90bca77e92fbb47396f2e5db2f3ec0dd8c4f8e78
Alex Goodwin <hagoodwi@gmail.com>
2020-10-16 12:00:37 -0700

    add validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :name, :cost, :country_of_origin, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :author, :content_body, :rating, presence: true
+  validates :rating, numericality: { only_integer: true , :less_than_or_equal_to => 5 }
 end
