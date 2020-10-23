2b9c20883492c9f8c0ff006a43e0c7f99d61f6f9
Alex Goodwin <hagoodwi@gmail.com>
2020-10-23 15:38:58 -0700

    add devise attempt

--- db/migrate/20201016164014_create_reviews.rb
+++ db/migrate/20201016164014_create_reviews.rb
@@ -1,5 +1,6 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :admin, :boolean, default: false
     create_table :reviews do |t|
       t.column(:author, :string)
       t.column(:content_body, :string)
