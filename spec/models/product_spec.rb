2acc9d2b8d9f865520922b9e8a0acf1a0ad1ede9
Alex Goodwin <hagoodwi@gmail.com>
2020-11-05 08:16:44 -0800

    add passing tests for titlization

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Product do
   it("titleizes the name of an product") do
-    product = Product.create({name: "alex"})
+    product = Product.create!({name: "alex", cost: 23, country_of_origin: "United States"})
     expect(product.name).to(eq("Alex"))
   end
 
